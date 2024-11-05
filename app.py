55205e357d31685195f952060c1c5ea2a5d04f8a
apaarmalik <144600507+apaarmalik@users.noreply.github.com>
2024-11-05 20:27:58 +0530

    app.py

--- app.py
+++ app.py
@@ -3,10 +3,4 @@ import pandas as pd
 import numpy as np
 
 num1 = st.numper_input("enter 1st number")
-num2 = st.number_input("enter 2nd number")
-
-num3 = num1 + num2
-
-
-st.write("hello")
-st.write(num3)
+st.write(num1)
