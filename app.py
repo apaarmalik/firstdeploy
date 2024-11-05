6e06bf41981afb81d8b073d95d3696c81dbd342a
apaarmalik <144600507+apaarmalik@users.noreply.github.com>
2024-11-05 20:17:14 +0530

    app.py

--- app.py
+++ app.py
@@ -1,2 +1,12 @@
 import streamlit as st
+import pandas as pd
+import numpy as np
+
+num1 = input("enter 1st number")
+num2 = input("enter 2nd number")
+
+num3 = num1 + num2
+
+
 st.write("hello")
+st.write(num3)
