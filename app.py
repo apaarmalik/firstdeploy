b2aeeb4d79e1a0ca6af63ed25e7b0fb25192a04d
apaarmalik <144600507+apaarmalik@users.noreply.github.com>
2024-11-05 20:24:30 +0530

    app.py

--- app.py
+++ app.py
@@ -2,8 +2,8 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 
-num1 = input("enter 1st number")
-num2 = input("enter 2nd number")
+num1 = st.numper_input("enter 1st number")
+num2 = st.number_input("enter 2nd number")
 
 num3 = num1 + num2
 
