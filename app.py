aff0c09f0a5fe1d262ae65c60361861582cc896f
apaarmalik <144600507+apaarmalik@users.noreply.github.com>
2024-11-05 21:27:18 +0530

    app.py

--- app.py
+++ app.py
@@ -10,13 +10,14 @@ num1=st.number_input("enter 1st number")
 num2=st.number_input("enter 2nd number")
 opr = st.chat_input("Enter the operator")
 
-if opr == "+":
-  st.write(num1+num2)
-elif opr == "*" or opr =="x":
-  st.write(num1*num2)
-elif opr == "/":
-  st.write(num1/num2)
-elif opr == "-":
-  st.write(num1 - num2)
+if opr<>"":
+    if opr == "+":
+      st.write(num1+num2)
+    elif opr == "*" or opr =="x":
+      st.write(num1*num2)
+    elif opr == "/":
+      st.write(num1/num2)
+    elif opr == "-":
+      st.write(num1 - num2)
 else:
   st.write(" ")
