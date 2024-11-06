4bda7ae017f5a6109ff47e748c7930031fc54110
apaarmalik <144600507+apaarmalik@users.noreply.github.com>
2024-11-07 01:11:34 +0530

    app.py

--- app.py
+++ app.py
@@ -23,6 +23,14 @@ df.describe(buf=buffer)
 s = buffer.getvalue()
 st.text(s)
 
+
+st.write("newline")
+
+df_aux = df.describe(include='all')
+st.write(df_aux)
+
+
+
 num1=st.number_input("enter 1st number")
 num2=st.number_input("enter 2nd number")
 opr = st.chat_input("Enter the operator")
