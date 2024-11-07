5ff90564ae94e8d21b36e091710d78aad277edfe
apaarmalik <144600507+apaarmalik@users.noreply.github.com>
2024-11-07 23:40:41 +0530

    app.py

--- app.py
+++ app.py
@@ -29,13 +29,17 @@ st.write("newline")
 df_aux = df.describe(include='all')
 st.write(df_aux)
 
-max = df["value"].max()
-st.write(f"The maximum value in the dataframe is {max}")
+
 
 tx = st.chat_input("Enter the text")
 ctx = tx.split()
-st.write(ctx[0])
 
+for i in range(len(ctx)):
+    if ctx[i]=="max":
+        max = df["value"].max()
+        st.write(f"The maximum value in the dataframe is {max}")
+    else:
+        st.write(f"Aska valid question")
 
 num1=st.number_input("enter 1st number")
 num2=st.number_input("enter 2nd number")
